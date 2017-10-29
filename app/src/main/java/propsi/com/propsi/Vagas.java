d442f9ca5d19cb117588a2f1afba20a8c248eb82
apdiegosilva <32199467+apdiegosilva@users.noreply.github.com>
2017-10-29 15:39:52 -0200

    Terminado tela Cursos

---
+++ app/src/main/java/propsi/com/propsi/Vagas.java
@@ -0,0 +1,13 @@
+package propsi.com.propsi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vagas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vagas);
+    }
+}
